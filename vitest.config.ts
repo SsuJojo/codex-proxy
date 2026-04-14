f61502fa4c21e2a7cfe3ada1c5c6af6984872f0d
SsuJo_ <1049731887@qq.com>
2026-04-14 17:22:57 +0800

    test: 添加简易测试 test:local-chat 只覆盖了各个模型的 openai 格式的 curl 请求

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       "shared/**/*.{test,spec}.ts",
       "tests/unit/**/*.{test,spec}.ts",
       "tests/integration/**/*.{test,spec}.ts",
+      "tests/local-chat/**/*.{test,spec}.ts",
       "packages/electron/__tests__/**/*.{test,spec}.ts",
     ],
   },
